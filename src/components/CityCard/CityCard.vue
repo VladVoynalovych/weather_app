a1a842fcf9b324a558fb52512defd6b5ac7b862b
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-12-11 23:04:15 +0200

    add( CityCard )

---
+++ src/components/CityCard/CityCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="city-card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CityCard"
+  }
+</script>
+
+<style scoped>
+  .city-card {
+    width: 80%;
+    min-height: 300px;
+    height: max-content;
+    box-shadow: 0 0 10px rgba(0, 121, 183, .3);
+  }
+</style>
