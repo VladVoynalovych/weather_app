76e82836395cb70e52c7b4e27a2bb4ab7aab932d
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-12-11 22:05:41 +0200

    update( router ): Layout, pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import defaultLayout from '../layouts/DefaultLayout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Layout',
+    component: defaultLayout,
+    children: [
+      {
+        path: '/',
+        name: 'Main',
+        component: () => import('../pages/MainPage.vue'),
+      },
+      {
+        path: '/favourites',
+        name: 'Main',
+        component: () => import('../pages/FavouritesPage'),
+      },
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
