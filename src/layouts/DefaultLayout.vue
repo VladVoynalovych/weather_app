17e8c1125383f762cda84abae1ac1f66460e0b8a
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-12-11 22:02:22 +0200

    add(DefaultLayout)

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <img :src="logo" alt="" class="logo">
+    <div class="buttons-wrapper">
+      <CardButton class="main" :button-text="'Main'"></CardButton>
+      <CardButton class="favourites" :button-text="'Favourites'"></CardButton>
+    </div>
+  </nav>
+</template>
+
+<script>
+// media
+import logo from '@/assets/logo.svg';
+// media
+
+// components
+import CardButton from "@/components/CardButton/CardButton";
+// components
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    CardButton,
+  },
+  data() {
+    return {
+      logo
+    }
+  }
+}
+</script>
+<style scoped>
+  .buttons-wrapper {
+    display: flex;
+    align-items: center;
+    width: 50%;
+    height: 35px;
+    margin-top: 10px;
+  }
+
+  .buttons-wrapper .card-button {
+    margin-right: 10px;
+  }
+</style>
