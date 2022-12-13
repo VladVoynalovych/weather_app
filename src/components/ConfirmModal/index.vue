8c130001e31766046b9edf7ddf38eff61ad84781
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-12-13 20:03:35 +0200

    add( ConfirmModal ): layout, functionality

---
+++ src/components/ConfirmModal/index.vue
@@ -0,0 +1,140 @@
+<template>
+  <section :class="['confirm-modal', { 'confirm-modal_opened': opened }]">
+    <div class="confirm-modal_content">
+      <p class="confirm-modal_title">{{confirmTitle}}</p>
+      <p class="confirm-modal_confirm-text">{{confirmText}}</p>
+
+      <div class="confirm-modal_actions">
+        <button
+          class="confirm-modal_action-button confirm-modal_action-button_disagree"
+          @click="confirmation(false)"
+        >
+          <span class="confirm-modal_button-text">DISAGREE</span>
+        </button>
+        <button
+          class="confirm-modal_action-button confirm-modal_action-button_agree"
+          @click="confirmation(true)"
+        >
+          <span class="confirm-modal_button-text">AGREE</span>
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "ConfirmModal",
+    props: {
+      confirmTitle: {
+        type: String,
+        default: 'Confirm Title'
+      },
+      confirmText: {
+        type: String,
+        default: 'Confirm Text'
+      },
+      opened: {
+        type: Boolean,
+        default: false
+      },
+      confirmValue: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      confirmation(confirmValue) {
+        this.$emit('update:confirmValue', confirmValue);
+        this.$emit('update:opened', false);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .confirm-modal {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(33, 33, 33, 0.46);
+    transform: scale(0);
+    transition: transform 0.1s;
+  }
+
+  .confirm-modal_opened {
+    transform: scale(1);
+  }
+
+  .confirm-modal_content {
+    max-width: 290px;
+    max-height: 250px;
+    width: 100%;
+    height: auto;
+    box-shadow: 0 3px 1px -2px rgba(0, 0, 0, .2),
+                0 2px 2px 0 rgba(0, 0, 0, .14),
+                0 1px 5px 0 rgba(0, 0, 0, .12);
+    background-color: #fff;
+    color: rgba(0,0,0,.87);
+    border-radius: 4px;
+  }
+
+  .confirm-modal_title,
+  .confirm-modal_confirm-text {
+    text-align: left;
+  }
+
+  .confirm-modal_title {
+    font-family: Roboto,sans-serif!important;
+    line-height: 2rem;
+    padding: 16px 24px 10px;
+    font-size: 1.5rem;
+    font-weight: 500;
+  }
+
+  .confirm-modal_confirm-text {
+    padding: 0 24px 20px;
+    font-size: .875rem;
+    font-weight: 400;
+    line-height: 1.375rem;
+    letter-spacing: .0071428571em;
+  }
+
+  .confirm-modal_actions {
+    padding: 8px 16px;
+    align-items: center;
+    display: flex;
+  }
+
+  .confirm-modal_action-button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+    border: none;
+    background: transparent;
+    flex: 0 0 auto;
+    outline: 0;
+    font-weight: 500;
+    padding: 0 8px;
+    height: 36px;
+    min-width: 64px;
+    color: #43a047;
+    font-size: .875rem;
+    transition: background-color 0.1s, color 0.1s;
+  }
+
+  .confirm-modal_action-button:active {
+    background-color: rgba(67, 160, 71, .45);
+    color: #ffffff;
+  }
+
+  .confirm-modal_action-button:hover {
+    background-color: rgba(67, 160, 71, .15);
+  }
+</style>
