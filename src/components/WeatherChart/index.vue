7b212a26898bbf5a99e514e9f220b97ec95ef4fa
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-12-13 20:05:19 +0200

    fix( WeatherChart ): type

--- src/components/WeatherChart/index.vue
+++ src/components/WeatherChart/index.vue
@@ -45,7 +45,7 @@
       const ctx = document.getElementById('myChart').getContext('2d');
 
       const myChart = new Chart(ctx, {
-        type: 'line',
+        type: this.type,
         data: {
           labels: this.labels,
           datasets: [{
