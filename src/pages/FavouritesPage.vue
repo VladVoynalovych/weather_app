3fffbc5bd782e7dbeb28d00e160b00b086990c6c
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-12-14 00:12:56 +0200

    update( FavouritesPage.vue ): layout

--- src/pages/FavouritesPage.vue
+++ src/pages/FavouritesPage.vue
@@ -1,13 +1,38 @@
 <template>
-  <div></div>
+  <article class="favourites-page">
+    <favourite-city-card
+      v-for="(cityCard, cityIndex) in getFavouritesContent"
+      :key="cityIndex"
+      :index="cityIndex"
+      :city-card="cityCard"
+    >
+    </favourite-city-card>
+  </article>
 </template>
 
 <script>
+  import FavouriteCityCard from "@/components/FavouriteCityCard";
+
+  import { mapGetters } from 'vuex';
+
   export default {
-    name: "FavouritesPage"
+    name: "FavouritesPage",
+    components: {
+      FavouriteCityCard,
+    },
+    computed: {
+      ...mapGetters('favouritesModule', ['getFavouritesContent'])
+    },
+    methods: {
+    },
   }
 </script>
 
 <style scoped>
-
+  .favourites-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 </style>
