ef2125bfb4f6a6da2d43b97ee088bc39f33ef32a
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-12-13 06:07:18 +0200

    update( MainPage.vue ): functionality

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="main-page">
-    <CityCard></CityCard>
-  </div>
+  <article class="main-page">
+    <city-card
+      v-for="(cityCard, cityIndex) in getWeatherContent"
+      :key="cityIndex"
+      :city-name="cityCard.cityName"
+      :weather-list="cityCard.weatherList"
+    >
+    </city-card>
+  </article>
 </template>
 
 <script>
   import CityCard from "@/components/CityCard/CityCard";
 
+  import { mapGetters } from 'vuex';
+
   export default {
     name: "MainPage",
     components: {
       CityCard,
+    },
+    computed: {
+      ...mapGetters('weatherModule', ['getWeatherContent'])
+    },
+    methods: {
+
     }
   }
 </script>
@@ -19,5 +33,7 @@
   .main-page {
     display: flex;
     justify-content: center;
+    align-items: center;
+    flex-direction: column;
   }
 </style>
